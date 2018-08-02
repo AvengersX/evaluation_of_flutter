da1c6f429a970c020651483e8586869e954d82af
liang <liang@sogou-inc.com>
2018-08-02 12:03:11 +0800

    set recycler view list item height to half of the height of recycler view.

--- PureAndroidImageViewer/app/src/main/java/sogou/com/pureandroidimageviewer/MainActivity.java
+++ PureAndroidImageViewer/app/src/main/java/sogou/com/pureandroidimageviewer/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.ViewTreeObserver;
 import android.widget.LinearLayout;
 import android.widget.Toast;
 
